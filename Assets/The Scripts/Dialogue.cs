530efeeca5b35d92cd7d3a58fb370daf521a4461
ThirdJupiter <disa.berntsson@gmail.com>
2022-05-24 16:10:05 +0200

    Dialogue
    Tried to do Dialog, see video in the word place

---
+++ Assets/The Scripts/Dialogue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue
+{
+    [SerializeField] string childe; //the child we talk with
+    [SerializeField] string[] sentences;
+}
---
+++ Assets/The Scripts/DialogueManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+
+    Queue<string> Sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sentences = new Queue<string>();
+    }
+
+    //public void StartDialogue(Dialogue dialogue);
+   
+    //{ 
+    //    Debug.Log("Starting Dialogue with " + dialogue.child); 
+    //}
+
+    
+ 
+}
