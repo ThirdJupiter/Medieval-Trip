2b20d959fe4564aa5df7be880634fd689b64d508
ThirdJupiter <disa.berntsson@gmail.com>
2022-05-31 15:45:43 +0200

    Tried to do dialogue
    Tried to do dialog, see the vid sent to sanam the 31 of may
    
    Also removed some stuff we dont use

--- Assets/The Scripts/Dialogue.cs
+++ Assets/The Scripts/Dialogue.cs
@@ -1,9 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Dialogue
+public class Dialogue : MonoBehaviour
 {
-    [SerializeField] string childe; //the child we talk with
-    [SerializeField] string[] sentences;
+    public TextMeshProUGUI textCompnent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    
+    void Start()
+    {
+        textCompnent.text = string.Empty;
+        StartDialogue(); //when to trigger?
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (textCompnent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textCompnent.text = lines[index];
+            }
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textCompnent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textCompnent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
