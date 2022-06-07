41371916fcd76811b603442ac7e3d60541a0ba77
ThirdJupiter <disa.berntsson@gmail.com>
2022-06-07 14:12:32 +0200

    Fixed Teleport stone
    And changed some dialog script, not done

--- Assets/The Scripts/Dialogue.cs
+++ Assets/The Scripts/Dialogue.cs
@@ -5,61 +5,109 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI textCompnent;
-    public string[] lines;
-    public float textSpeed;
+    [SerializeField] TextMeshProUGUI text;
+    float timer = 0;
 
-    private int index;
+    [SerializeField] string[] responses;
+    int responseIndex = 0;
 
-    void StartDialogue()
+    public void ShowMessage(string message, float time)
     {
-        index = 0;
-        StartCoroutine(TypeLine());
+        text.text = message;
+        timer = time;
     }
     
     void Start()
     {
-        textCompnent.text = string.Empty;
-        StartDialogue(); //when to trigger?
+
     }
 
-  
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (timer > 0)
         {
-            if (textCompnent.text == lines[index])
+            timer -= Time.deltaTime;
+            if (timer < 0)
             {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textCompnent.text = lines[index];
+                text.text = "";
             }
         }
+
+    }
+    public string GetResponse()
+    {
+        if (responses == null || responses.Length < 1) return "...";
+
+        string response = responses[responseIndex];
+        responseIndex++;
+        if (responseIndex >= responses.Length) responseIndex = 0;
+        return response;
     }
 
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textCompnent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
 
-    void NextLine()
-    {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            textCompnent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
-    }
 }
+
+
+
+
+
+//{
+//    public TextMeshProUGUI textCompnent;
+//    public string[] lines;
+//    public float textSpeed;
+
+//    private int index;
+
+//    void StartDialogue()
+//    {
+//        index = 0;
+//        StartCoroutine(TypeLine());
+//    }
+
+//    void Start()
+//    {
+//        textCompnent.text = string.Empty;
+//        StartDialogue(); //when to trigger?
+//    }
+
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Space)) 
+//        {
+//            if (textCompnent.text == lines[index])
+//            {
+//                NextLine();
+//            }
+//            else
+//            {
+//                StopAllCoroutines();
+//                textCompnent.text = lines[index];
+//            }
+//        }
+//    }
+
+//    IEnumerator TypeLine()
+//    {
+//        foreach (char c in lines[index].ToCharArray())
+//        {
+//            textCompnent.text += c;
+//            yield return new WaitForSeconds(textSpeed);
+//        }
+//    }
+
+//    void NextLine()
+//    {
+//        if (index < lines.Length - 1)
+//        {
+//            index++;
+//            textCompnent.text = string.Empty;
+//            StartCoroutine(TypeLine());
+//        }
+//        else
+//        {
+//            gameObject.SetActive(false);
+//        }
+//    }
+
--- Assets/The Scripts/wasd.cs
+++ Assets/The Scripts/wasd.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class wasd : MonoBehaviour
 {
+
+
     [SerializeField] Rigidbody2D rb;
     public float speed = 10f;
     // public PointEffector2D p;
-    
+    List<ConversationSkelly> convoBoys = new List<ConversationSkelly>();
+    [SerializeField] Dialogue ui;
 
     // Start is called before the first frame update
     void Start()
@@ -35,4 +38,40 @@ public class wasd : MonoBehaviour
 
     }
     
+
+    private void TalkTo()
+    {
+        if (convoBoys.Count < 1) return;
+        Child c;
+        float closestDistance = float.MaxValue;
+        int closestIndex = 0;
+        for (int i = 0; i < convoBoys.Count; i++)
+        {
+            float d = Vector3.Distance(transform.position, convoBoys[i].transform.position);
+            if (d < closestDistance)
+            {
+                closestDistance = d;
+                closestIndex = i;
+            }
+        }
+
+        ui.ShowMessage(convoBoys[closestIndex].GetResponse(), 1.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ConversationSkelly sk;
+        if (collision.TryGetComponent<ConversationSkelly>(out sk))
+        {
+            convoBoys.Add(sk);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ConversationSkelly sk;
+        if (collision.TryGetComponent<ConversationSkelly>(out sk))
+        {
+            convoBoys.Remove(sk);
+        }
+    }
 }
